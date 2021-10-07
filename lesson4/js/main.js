431938d432b37e467a0106722706aaed6dba4f82
msander87 <msander1987@gmail.com>
2021-10-07 18:46:28 -0300

    changes

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -10,3 +10,8 @@ catch(e){
     alert("error with code or your browser does not support Locale");
 }
 
+const menubutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.nonResponsive')
+
+menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
