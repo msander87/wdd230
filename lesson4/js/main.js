05c38b0fc848068612ef7e1493d6a8252e9f62f1
msander87 <msander1987@gmail.com>
2021-10-07 16:42:24 -0300

    creating

---
+++ lesson4/js/main.js
@@ -0,0 +1,12 @@
+
+try{
+    const options = { 
+        year: 'numeric'        
+    }; 
+    document.getElementById("year").textContent = new Date().toLocaleDateString("en-US", options);
+
+}
+catch(e){
+    alert("error with code or your browser does not support Locale");
+}
+
