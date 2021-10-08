9086330374037b493e0ed76a99ce1947761c60d3
msander87 <msander1987@gmail.com>
2021-10-08 01:36:55 -0300

    last touches

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -18,8 +18,12 @@ catch(e){
     alert("error with code or your browser does not support Locale");
 }
 
-const menubutton = document.querySelector('.menu');
-const mainnav = document.querySelector('.nonResponsive')
 
-menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
+function openClose(){
+    const mainnav = document.querySelector('.nonResponsive');
+    const menup = document.querySelector('.open');
+    
+    mainnav.classList.toggle('responsive');
+    menup.classList.toggle('close');
+}
