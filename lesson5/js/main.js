f3bc4caefd0157083c4103c06ef1dc35d1d90ea3
msander87 <msander1987@gmail.com>
2021-10-13 18:11:35 -0300

    uploading

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -1,12 +1,39 @@
 
-try{
-    const options = { 
-        year: 'numeric'        
-    }; 
-    document.getElementById("year").textContent = new Date().toLocaleDateString("en-US", options);
+  const list = document.querySelector('ul');
+  const input = document.querySelector('#favchap');
+  const button = document.querySelector('button');
+  const mainElement = document.querySelector('main'); 
+  const errorP = document.createElement('p');
+  
+  button.addEventListener('click', function() { 
+      
+    let fav = input.value;
+    if(fav == ""){
+        errorP.innerHTML = "please enter a book and chapter"
+        errorP.setAttribute('class', 'error-p')
+        mainElement.appendChild(errorP);
+        
+    }
+    else{        
+        input.value = '';
+        
+        const liElement = document.createElement('li');
+        const deleteBtn = document.createElement('button');
+        const liText = document.createElement('span');
 
-}
-catch(e){
-    alert("error with code or your browser does not support Locale");
-}
+        liElement.appendChild(liText);
+        liText.innerHTML = fav;
+        liElement.appendChild(deleteBtn);
+        deleteBtn.innerHTML = '&#x2716;';
+        deleteBtn.setAttribute('class', 'del-btn');
+        list.appendChild(liElement);
 
+        
+        deleteBtn.addEventListener('click', function() { 
+            list.removeChild(liElement);
+        });
+    
+        input.focus();
+        mainElement.removeChild(errorP);
+    }
+  });
