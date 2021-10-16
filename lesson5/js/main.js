638e9ef9e0e8547a9e5ab581278c83165529065f
msander87 <msander1987@gmail.com>
2021-10-16 16:39:46 -0300

    finished week 5

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -18,7 +18,17 @@ catch(e){
     alert("error with code or your browser does not support Locale");
 }
 
+let today = new Date();
+let dayOfWeek = today.getDay();
 
+if(dayOfWeek == 5){
+    document.querySelector(".sb-container").style.display = 'block';
+}
+
+/*
+let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Sat', 'Sun']
+let numbers = [0, 1, 2, 3, 4, 5, 6]
+*/
 
 function openClose(){
     const mainnav = document.querySelector('.nonResponsive');
