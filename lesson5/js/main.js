8e0eec902cec79d4f150e12c7bfef5951a2c24f9
msander87 <msander1987@gmail.com>
2021-10-17 02:15:27 -0300

    final changes

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -18,6 +18,8 @@ catch(e){
     alert("error with code or your browser does not support Locale");
 }
 
+
+//to show the banner only on Fridays
 let today = new Date();
 let dayOfWeek = today.getDay();
 
@@ -25,11 +27,22 @@ if(dayOfWeek == 5){
     document.querySelector(".sb-container").style.display = 'block';
 }
 
-/*
+//to set the name of the days in the forecaste section
 let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Sat', 'Sun']
-let numbers = [0, 1, 2, 3, 4, 5, 6]
-*/
+let numbersDay = [];
+let thisDay = dayOfWeek;
+for (let i = 0; i < 5; i++ ){
+    thisDay = thisDay + 1;
+    numbersDay.push(thisDay);
+}
+document.querySelector("#h51").innerHTML = days[numbersDay[0]];
+document.querySelector("#h52").innerHTML = days[numbersDay[1]];
+document.querySelector("#h53").innerHTML = days[numbersDay[2]];
+document.querySelector("#h54").innerHTML = days[numbersDay[3]];
+document.querySelector("#h55").innerHTML = days[numbersDay[4]];
 
+
+//function to open and close de hamburguer menu
 function openClose(){
     const mainnav = document.querySelector('.nonResponsive');
     const menup = document.querySelector('.open');
