01d0898ad48a849915da162e9dad5d7779e22ef0
msander87 <msander1987@gmail.com>
2021-11-09 15:08:28 -0300

    changes

--- lesson-9/prophets/js/prophets.js
+++ lesson-9/prophets/js/prophets.js
@@ -1,11 +1,11 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-
+let prophets = [];
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    prophets = jsonObject['prophets'];
     for (let i = 0; i < prophets.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
@@ -14,8 +14,7 @@ fetch(requestURL)
 
         card.appendChild(h2);
 
-        document.querySelector('div.cards').appendChild(card);
+        document.querySelector('.cards').appendChild(card);
     }
   });
 
-let prophets = jsonObject['prophets'];  
