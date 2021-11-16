fbe420527102c5e4664a57d719a8b1b5acbe7bac
msander87 <msander1987@gmail.com>
2021-11-16 15:00:15 -0300

    finished api activity

--- lesson-10/api/js/weatherapi.js
+++ lesson-10/api/js/weatherapi.js
@@ -1,7 +1,17 @@
-const apiURL = "api.openweathermap.org/data/2.5/weather?id=2172797&appid=2ace1636d2b567ce42e688891813d59d";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=2ace1636d2b567ce42e688891813d59d";
+
+
+
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     document.querySelector("#current-temp").innerHTML = jsObject.main.temp;
-    alert(tem);
+    
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description; 
+    document.querySelector('#imagesrc').textContent = imagesrc;
+    document.querySelector('#icon').setAttribute('src', imagesrc); 
+    document.querySelector('#icon').setAttribute('alt', desc);
+    document.querySelector('#name').innerHTML = jsObject.name;
+    document.querySelector('#json').innerHTML = JSON.stringify(jsObject);
   });
