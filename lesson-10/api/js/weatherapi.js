c80fb965de8e176fa5a7bf884580fa01443a1b12
msander87 <msander1987@gmail.com>
2021-11-16 14:21:45 -0300

    changes

--- lesson-10/api/js/weatherapi.js
+++ lesson-10/api/js/weatherapi.js
@@ -1,35 +1,7 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-let prophets = [];
-
-function createCard(prophetsList){
-    prophetsList.forEach(prophet => {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let pBirthdate = document.createElement("p");
-        let pBirthplace = document.createElement("p");
-        let image = document.createElement("img");
-
-        h2.textContent = `${prophet.name} ${prophet.lastname}`;
-        pBirthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
-        pBirthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
-        image.setAttribute('src', prophet.imageurl);
-        image.setAttribute('alt', `${prophet.name} ${prophet.lastname} - ${prophet.order}`);
-
-
-        card.appendChild(h2);
-        card.appendChild(pBirthdate);
-        card.appendChild(pBirthplace);
-        card.appendChild(image);
-
-        document.querySelector('div.cards').appendChild(card);        
-    });
-}
-
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    prophets = jsonObject['prophets'];
-    createCard(prophets);
+const apiURL = "api.openweathermap.org/data/2.5/weather?id=2172797&appid=2ace1636d2b567ce42e688891813d59d";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    let tem = jsObject.main.temp;
+    alert(tem);
   });
