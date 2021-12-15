5e909b922fc8fb2ddb560bd0662f64136cbf0fe6
msander87 <msander1987@gmail.com>
2021-12-15 05:45:19 -0300

    rivera almost finished

--- final-project-rivera/js/city_data.js
+++ final-project-rivera/js/city_data.js
@@ -6,9 +6,14 @@ fetch(apiURL)
   .then((jsObject) => {
    
     document.querySelector('#motto').innerHTML = jsObject.info.motto;
-    document.querySelector('#founded').innerHTML = `It was founded in: ${jsObject.info.founded}`;
+    document.querySelector('#founded').innerHTML = `It was founded in ${jsObject.info.founded}`;
     document.querySelector('#population').innerHTML = `Current population: ${jsObject.info.population}`;
-    document.querySelector('#governor').innerHTML = `Current governor: ${jsObject.governor}`;  
+    document.querySelector('#governor').innerHTML = `Current governor: ${jsObject.info.governor}`;  
+
+    document.querySelector('#active').innerHTML = `Active cases: ${jsObject.covid.active}`;
+    document.querySelector('#recovered').innerHTML = `Recovered people: ${jsObject.covid.recovered}`;
+    document.querySelector('#vaccinated').innerHTML = `Vaccinated people: ${jsObject.covid.vaccinated}`;
+    document.querySelector('#deaths').innerHTML = `Confirmed deaths: ${jsObject.covid.deaths}`;  
     
   
   });
